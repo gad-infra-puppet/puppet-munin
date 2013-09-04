ee3668402d22ad772436dffaf890fd539155a928
Mingxiang.Zhang <ming.zhang@gree.net>
2013-09-04 15:05:55 +0900

    change interface name from bond0_915=>bond0.915

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -1,8 +1,11 @@
 # handle if_ and if_err_ plugins
 class munin::plugins::interfaces  {
 
+  # only for bitv2: change bond0_xxx => bond0.xxx
+  $fixed_interfaces = regsubst($::interfaces, 'bond0_([0-9]+)', 'bond0.\1', 'G')
+
   # filter out many of the useless interfaces that show up
-  $real_ifs = reject(split($::interfaces, ' |,'), 'eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+|__tmp\d+')
+  $real_ifs = reject(split($fixed_interfaces, ' |,'), 'eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+|__tmp\d+')
   $ifs = regsubst($real_ifs, '(.+)', "if_\\1")
 
   munin::plugin {
