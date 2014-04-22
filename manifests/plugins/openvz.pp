d063979e237d305043f04faaa831ce025b2f3126
Mingxiang.Zhang <ming.zhang@gree.net>
2014-04-22 14:55:37 +0900

    munin: openvz_ munin pluings

--- manifests/plugins/openvz.pp
+++ manifests/plugins/openvz.pp
@@ -1,14 +1,25 @@
 class munin::plugins::openvz {
-  munin::plugin::deploy {
+  package { ['libcolor-calc-perl', 'libgraphics-colorobject-perl']:
+    ensure => "present";
+  }
+
+  include munin::plugin::scriptpaths
+  file { "munin_plugin_openvz_":
+    path    => "${munin::plugin::scriptpaths::script_path}/openvz_",
+    source  => "puppet:///modules/munin/plugins/openvz_",
+    owner   => root,
+    group   => 0,
+    mode    => '0755';
+  }
+
+  munin::plugin {
     [ 'openvzcpu' ]:
       config => "user root\nenv.drawidle 1",
       require => Package['libcolor-calc-perl', 'libgraphics-colorobject-perl'];
     ['openvz_physpages', 'openvz_laverage', 'openvz_status']:
-      source => 'openvz_',
-      config => 'user root'
+      ensure => 'openvz_',
+      config => 'user root',
+      require => File['munin_plugin_openvz_'];
   }
 
-  package { ['libcolor-calc-perl', 'libgraphics-colorobject-perl']:
-    ensure => "present";
-  }
 }
